8b1c1cf5ad33a617c17fe415ab400a7dae9ff140
MuhammadSalah99 <mohmmedjunide73@gmail.com>
2023-05-04 21:54:50 +0300

    new changes

--- src/pages/dashboard/pagebuilder/UserElem/Container.tsx
+++ src/pages/dashboard/pagebuilder/UserElem/Container.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Paper } from '@material-ui/core'
-const Container = ({background, padding = 0, children}: {background: string, padding: number, children: string}) => {
+const Container = ({background, padding = 0, children}: {background: string, padding: number, children: any}) => {
   return (
     <Paper style={{margin: "5px 0", background, padding: `${padding}px`}}>
-      {children}
+        {children}
     </Paper>
   )
 }
