39147cbddfc30c156ddf6fbe5b7e9cd6c935657c
MuhammadSalah99 <mohmmedjunide73@gmail.com>
2023-06-11 07:54:39 +0300

    changes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -29,9 +29,9 @@ const Home = () => {
     }, []);
     return (
 
-        <div className='w-screen bg-gray-50 dark:bg-gray-900 h-screen'>
+        <div className='w-screen bg-gray-50 dark:bg-gray-900'>
             <Navbar />
-            <div className='flex w-full p-12 flex-col  '>
+            <div className='flex w-full p-12 flex-col h-full '>
                 {engs.map((eng) => (
                     <Link to="#" className="flex flex-col mb-3 w-2/3 h-[200px] items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row relative hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                         <img className="object-cover w-full rounded-t-lg h-full  md:w-48 md:rounded-none md:rounded-l-lg" src={eng.profilePic} alt="" />
